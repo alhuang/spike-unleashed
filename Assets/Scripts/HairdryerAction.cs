50bd7119e4e72311e2c7217f0f9c1763f7479a04
EtherealAurora <zaaron@umich.edu>
2018-03-17 14:01:23 -0400

    player action

---
+++ Assets/Scripts/HairdryerAction.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HairdryerAction : MonoBehaviour {
+
+    public GameObject destinationObj;
+    private bool act = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (act)
+        {
+            if (gameObject.transform.position == destinationObj.transform.position)
+                act = false;
+            else
+                transform.position = Vector3.Lerp(transform.position, destinationObj.transform.position, 6 * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerAction pa = collision.gameObject.GetComponent<PlayerAction>();
+        if (pa != null)
+        {
+            pa.spaceClicked += moveToDestination;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        PlayerAction pa = collision.gameObject.GetComponent<PlayerAction>();
+        if (pa != null)
+        {
+            pa.spaceClicked -= moveToDestination;
+        }
+    }
+
+    private void moveToDestination()
+    {
+        act = true;
+    }
+
+}
