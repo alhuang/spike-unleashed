a1ce673a3cc00b6f7b4468a84f27619982cfa6a0
Jarred McDuffey <jarredmcduffey@0587349214.wireless.umich.net>
2018-03-17 16:01:00 -0400

    Enemy movement in progress

---
+++ Assets/Scripts/Direction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Direction : MonoBehaviour {
+
+	public string direction = "Right";
+
+	public string Direct()
+	{
+		return direction;
+	}
+}
---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour {
+
+	private bool initial = true;
+	private bool stopped = true;
+	public string direction = "Right";
+	public float moveSpeed = 4; 
+
+	Direction direct;
+	Rigidbody2D rigid;
+
+	// Use this for initialization
+	void Start () {
+		rigid = this.GetComponent<Rigidbody2D> ();
+		StartCoroutine (Initial ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!stopped) {
+			Vector3 position = rigid.velocity;
+			if (direction == "Right") {
+				position = new Vector3(moveSpeed, 0, 0);
+			} else if(direction == "Left") {
+				position = new Vector3(-moveSpeed, 0, 0);
+			} else if(direction == "Up") {
+				position = new Vector3(0, moveSpeed, 0);
+			} else if(direction == "Down") {
+				position = new Vector3(0, -moveSpeed, 0);
+			}
+
+			rigid.velocity = position;
+		}
+	}
+
+	IEnumerator Initial()
+	{
+		yield return new WaitForSeconds (2);
+		stopped = false;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		print ("here");
+		if (LayerMask.LayerToName (other.gameObject.layer) == "Direction") {
+			direct = other.gameObject.GetComponent<Direction> ();
+			direction = direct.Direct ();
+		} else if (LayerMask.LayerToName (other.gameObject.layer) == "Stop") {
+			stopped = true;
+		}
+	}
+
+
+}
