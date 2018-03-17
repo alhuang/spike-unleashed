33f26c84778a3567e4f39e9279b7b195e8cacd39
Jarred McDuffey <jarredmcduffey@0587349214.wireless.umich.net>
2018-03-16 21:02:39 -0400

    Added movement and Animations

---
+++ Assets/Scripts/MoveAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAnimation : MonoBehaviour {
+
+	Animator anim; 
+
+	// Use this for initialization
+	void Start () {
+		anim = this.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	Rigidbody2D rigid;
+	public float moveSpeed = 5;
+
+	// Use this for initialization
+	void Awake () {
+		rigid = this.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float xInput = Input.GetAxisRaw ("Horizontal");
+		float yInput = Input.GetAxisRaw ("Vertical");
+
+		Vector2 velocity = new Vector2 (xInput, yInput);
+		velocity *= moveSpeed;
+
+		rigid.velocity = velocity;
+	}
+}
