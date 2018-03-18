5492d02caff798f3344b1db0e96a6405a622bcce
Jarred McDuffey <jarredmcduffey@0587349214.wireless.umich.net>
2018-03-17 21:28:38 -0400

    Enemy movement is finished

--- Assets/Scripts/Direction.cs
+++ Assets/Scripts/Direction.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Direction : MonoBehaviour {
 
 	public bool second = false;
+	public bool three = false;
 	public string direction = "Right";
 	public string nextDirect = "Down";
 
@@ -16,6 +17,15 @@ public class Direction : MonoBehaviour {
 				return direction;
 			} else {
 				return nextDirect;
+			}
+		} else if (three) {
+			int val = Random.Range (0, 3);
+			if (val == 0) {
+				return direction;
+			} else if( val == 1){
+				return nextDirect;
+			} else{
+				return "Down";
 			}
 		}
 		return direction;
