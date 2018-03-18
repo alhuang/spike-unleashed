6697b57e7f3f0101c643afa9beb94e80082f3afb
Emily Siu <emsiu@umich.edu>
2018-03-17 23:37:02 -0400

    Human can catch dog

--- Assets/Scripts/CatchDog.cs
+++ Assets/Scripts/CatchDog.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CatchDog : MonoBehaviour {
 
@@ -17,6 +18,16 @@ public class CatchDog : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Dog")) {
             Debug.Log("Dog!");
+            StartCoroutine(ResetDog(other));
+
         }
+    }
+
+    IEnumerator ResetDog(Collider2D other) {
+        PanelController.instance.Human("Spike! How did you get out of your pen?");
+        yield return new WaitForSeconds(3f);
+        PanelController.instance.Human("Nevermind that. Let's get you back.");
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("Main");
     }
 }
